a6b566bca5e5b2ef7e908c2b7b3d13d73d29fc6b
Christian Parker <christianparker512@gmail.com>
2021-06-11 13:07:06 -0500

    resetting the value of the name form

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
 const app = Vue.createApp({
-  data(){
+  data() {
     return {
-      courseGoalA:'Finish the course and learn Vue',
-      courseGoalB:'Master Vue and build amazing apps',
-      vueLink: 'https://vuejs.org'
+      counter: 0,
+      name: ''
     };
   },
   methods: {
-    outputGoal(){
-      const randomNumber = Math.random();
-      if(randomNumber < 0.5) {
-        return this.courseGoalA;
-      } else {
-        return this.courseGoalB;
-      }
+    setName(event, lastName) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput(){
+      this.name='';
     }
   }
 });
 
-app.mount('#user-goal');
+app.mount('#events');
